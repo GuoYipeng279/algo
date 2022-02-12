af11002863fd7ba944152e5307bc9a922e6ef9f8
GuoYipeng279 <yg2719@ic.ac.uk>
2022-02-12 20:47:20 +0000

    cut_pdf

---
+++ pdf.py
@@ -0,0 +1,18 @@
+from PyPDF2 import PdfFileWriter, PdfFileReader
+
+with open("89.pdf", "rb") as in_f:
+    input1 = PdfFileReader(in_f)
+    output = PdfFileWriter()
+
+    numPages = input1.getNumPages()
+    print("document has %s pages." % numPages)
+
+    for i in range(numPages):
+        page = input1.getPage(i)
+        print(page.mediaBox.getUpperRight_x(), page.mediaBox.getUpperRight_y())
+        page.cropBox.lowerLeft = (80, 70)
+        page.cropBox.upperRight = (550, 700)
+        output.addPage(page)
+
+    with open("out.pdf", "wb") as out_f:
+        output.write(out_f)
